dd08307e47973e2ea286260106e448338175d285
mutwiri-2 <devmutwiri2@gmail.com>
2020-02-09 18:49:52 +0300

    count number of vowels in a string

---
+++ fundamentals/vowel_count.py
@@ -0,0 +1,19 @@
+# Return the number (count) of vowels in the given string.
+
+# We will consider a, e, i, o, and u as vowels for this Kata.
+
+# The input string will only consist of lower case letters and/or spaces.
+
+def get_count(input_str):
+    num_vowels = 0
+    for letter in input_str:
+        if letter in ('a','e','i','o','u'):
+            num_vowels += 1
+    
+    return num_vowels
+
+print(get_count('learn by practice the github pull request workflow'))
+print(get_count('fork this template for the 100 days journal - to keep yourself accountable (multiple languages available)'))
+print(get_count('solutions from my python3 code practice challenges from hackerrank'))
+print(get_count('code practice from my freecodecamp study'))
+
