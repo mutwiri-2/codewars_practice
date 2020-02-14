decedfdfe2de04150bc1c8bb98634201ab733412
mutwiri-2 <devmutwiri2@gmail.com>
2020-02-14 14:34:41 +0300

    write number in expanded form

---
+++ fundamentals/number_in_expanded_form.py
@@ -0,0 +1,23 @@
+# Write Number in Expanded Form
+# You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+# expanded_form(12) # Should return '10 + 2'
+# expanded_form(42) # Should return '40 + 2'
+# expanded_form(70304) # Should return '70000 + 300 + 4'
+# NOTE: All numbers will be whole numbers greater than 0.
+
+def expanded_form(num):
+    num_str = str(num)
+    size = len(num_str)
+    result = []
+    for i in range(0,size):
+        if num_str[i] == '0':
+            continue
+        else:
+            result.append(num_str[i] + '0'*((size-1)-i))
+    return ' + '.join(result)
+    
+
+print(expanded_form(12))
+print(expanded_form(42))
+print(expanded_form(70304))
