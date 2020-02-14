6136f1111b450f86b9ab3739fc463c1272347d25
mutwiri-2 <devmutwiri2@gmail.com>
2020-02-14 11:36:39 +0300

    is a list sorted and how is it sorted

---
+++ fundamentals/sorted_yes_no_how.py
@@ -0,0 +1,20 @@
+# Complete the method which accepts an array of integers, and returns one of the following:
+
+# "yes, ascending" - if the numbers in the array are sorted in an ascending order
+# "yes, descending" - if the numbers in the array are sorted in a descending order
+# "no" - otherwise
+# You can assume the array will always be valid, and there will always be one correct answer.
+
+def is_sorted_and_how(arr):
+    if max(arr) == arr[-1] and min(arr) == arr[0]:
+        return 'yes, ascending'
+    elif min(arr) == arr[-1] and max(arr) == arr[0]:
+        return 'yes, descending'
+    else:
+        return 'no'
+
+print(is_sorted_and_how([1, 2]))
+
+print(is_sorted_and_how([15, 7, 3, -8]))
+
+print(is_sorted_and_how([4, 2, 30]))
