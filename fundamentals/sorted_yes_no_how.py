0551971d54bcb752e6004286e936540d38f6965b
mutwiri-2 <devmutwiri2@gmail.com>
2020-02-14 11:38:02 +0300

    alternative solution

--- fundamentals/sorted_yes_no_how.py
+++ fundamentals/sorted_yes_no_how.py
@@ -13,6 +13,13 @@ def is_sorted_and_how(arr):
     else:
         return 'no'
 
+def is_sorted_and_how(arr):
+    if arr == sorted(arr):
+        return 'yes, ascending'
+    elif arr == sorted(arr, reverse=True):
+        return 'yes, descending'
+    return 'no'
+
 print(is_sorted_and_how([1, 2]))
 
 print(is_sorted_and_how([15, 7, 3, -8]))
