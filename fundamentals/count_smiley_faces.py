bd2db56c060473a0918bb9ddabe8b8fd3ee3049b
mutwiri-2 <devmutwiri2@gmail.com>
2020-02-17 12:11:17 +0300

    alternate solution

--- fundamentals/count_smiley_faces.py
+++ fundamentals/count_smiley_faces.py
@@ -37,6 +37,18 @@ def count_smileys(arr):
         count += 1
     return count
 
+def count_smileys(arr):
+    eyes = [":", ";"]
+    noses = ["", "-", "~"]
+    mouths = [")", "D"]
+    count = 0
+    for eye in eyes:
+        for nose in noses:
+            for mouth in mouths:
+                face = eye + nose + mouth
+                count += arr.count(face)
+    return count
+
 print(count_smileys([]))
 print(count_smileys([':D',':~)',';~D',':)']))
 print(count_smileys([':)',':(',':D',':O',':;']))
