6013f7336f132aca3508a50fb127e343d17aa093
mutwiri-2 <devmutwiri2@gmail.com>
2020-02-18 20:48:31 +0300

    initial solution with bug

---
+++ fundamentals/sort_the_odd.py
@@ -0,0 +1,23 @@
+# You have an array of numbers.
+# Your task is to sort ascending odd numbers but even numbers must be on their places.
+
+# Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+
+# Example
+
+# sort_array([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
+
+
+def sort_array(source_array):
+    for num1, index1 in enumerate(source_array):
+        for num2, index2 in enumerate(source_array):
+            if num2 == 0 or num2 % 2 == 0 or num2 == num1:
+                continue
+            elif num1 <= num2:
+                source_array[index1] = num2
+                source_array[index2] = num1
+                    
+        return source_array
+
+print(sort_array([5, 3, 2, 8, 1, 4]))
+
