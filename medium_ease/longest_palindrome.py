302575505a6be8f92a2699b653160c7a2c2d120f
mutwiri-2 <devmutwiri2@gmail.com>
2020-02-23 20:17:30 +0300

    longest palindrome question

---
+++ medium_ease/longest_palindrome.py
@@ -0,0 +1,25 @@
+# Longest Palindrome
+# Find the length of the longest substring in the given string s that is the same in reverse.
+
+# As an example, if the input was “I like racecars that go fast”, the substring (racecar) length would be 7.
+
+# If the length of the input string is 0, the return value must be 0.
+
+# Example:
+# "a" -> 1 
+# "aab" -> 2  
+# "abcde" -> 1
+# "zzbaabcd" -> 4
+# "" -> 0
+
+def longest_palindrome (s):
+    list = []
+    for char in s:
+        
+
+print(longest_palindrome("a") #1
+print(longest_palindrome("aa") #2
+print(longest_palindrome("baa") #2
+print(longest_palindrome("aab") #2
+print(longest_palindrome("abcdefghba") #1
+print(longest_palindrome("baablkj12345432133d") #9
