408717a82830d4e21a237b907555491fcfd60276
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-26 01:21:28 +0300

    Python ternary operator

--- fundamentals/return_negative.py
+++ fundamentals/return_negative.py
@@ -23,3 +23,12 @@ print(make_negative(0),0)
 # using the built-in function abs() which returns the absolute value of the argument(value of argument without a sign), then we negate the answer.
 def make_negative(num):
     return -abs(num)
+
+# another solution using ternary conditional operator --> a if condition else b
+#Ternary operators also known as conditional expressions are operators that evaluate something based on a condition being true or false. It was added to Python in version 2.5. It simply allows to test a condition in a single line replacing the multiline if-else making the code compact. - https://www.geeksforgeeks.org/ternary-operator-in-python/
+def make_negative( number ):
+    return -number if number>0 else number
+
+print(make_negative(1), -1)
+print(make_negative(-5), -5)
+print(make_negative(0),0)
