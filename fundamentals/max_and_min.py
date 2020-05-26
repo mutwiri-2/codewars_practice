369d0b4722f44fad5a7d4ced69a9d1df7be6fb66
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-26 12:04:45 +0300

    define maximum and minimum functions

---
+++ fundamentals/max_and_min.py
@@ -0,0 +1,32 @@
+# Your task is to make two functions, max and min (maximum and minimum in PHP and Python) that take a(n) array/vector of integers list as input and outputs, respectively, the largest and lowest number in that array/vector.
+
+# #Examples
+
+# maximum([4,6,2,1,9,63,-134,566]) returns 566
+# minimum([-52, 56, 30, 29, -54, 0, -110]) returns -110
+# maximum([5]) returns 5
+# minimum([42, 54, 65, 87, 0]) returns 0
+
+# #Notes
+# You may consider that there will not be any empty arrays/vectors.
+
+def minimum(arr):
+    smallest = arr[0]
+    for i in arr[1:]:
+        if i<smallest:
+            smallest = i
+    return smallest
+
+def maximum(arr):
+    largest = arr[0]
+    for i in arr[1:]:
+        if i>largest:
+            largest = i
+    return largest
+
+print(maximum([4,6,2,1,9,63,-134,566])) # returns 566
+print(minimum([-52, 56, 30, 29, -54, 0, -110])) #returns -110
+print(maximum([5])) #returns 5
+print(minimum([42, 54, 65, 87, 0])) #returns 0
+
+print('#' * 99)
