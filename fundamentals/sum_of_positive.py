2c79951b4b8cc9fd5dc94e685d1927d317c04209
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-26 12:39:05 +0300

    use map and lambda to generate an iterator of +ve numbers

--- fundamentals/sum_of_positive.py
+++ fundamentals/sum_of_positive.py
@@ -28,9 +28,20 @@ print(positive_sum([]))
 print('#' * 99)
 
 # use lambda expression with filter higher order function to return an iterator with positive numbers only then pass them to sum built-in function
-# ilter is a higher order built-in function that takes in a function and an iterable as arguments and returns an iterator with the elements from the iterable for which the function returns True
+# filter is a higher order built-in function that takes in a function and an iterable as arguments and returns an iterator with the elements from the iterable for which the function returns True
 def positive_sum(arr):
     return sum(filter(lambda x: x>0 ,arr))  
+
+print(positive_sum([1,-4,7,12]))
+print(positive_sum([-2,-4,-8,-12]))
+print(positive_sum([]))
+
+print('#' * 99)
+
+# use lambda expression with map higher order function to return an iterator that applies the function to every element in the array then pass it to the sum built-in function
+# map() is a higher order built-in function that takes in a function and an iterable as arguments and returns an iterator that applies the function to every element in the iterable
+def positive_sum(arr):
+    return sum(map(lambda x: x if x>0 else 0, arr))  
 
 print(positive_sum([1,-4,7,12]))
 print(positive_sum([-2,-4,-8,-12]))
